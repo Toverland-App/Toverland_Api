d66ed83a9858023f51a55321799c9f4b882e6fde
nickvanhooff <nickvanhooff03@gmail.com>
2024-12-04 09:52:48 +0100

    Add Attractions property to Area class with JsonIgnore
    Introduced a new property `Attractions` to the `Area` class, which holds a list of `Attraction` objects. This property is decorated with the `[JsonIgnore]` attribute to exclude it from JSON serialization. Added the `System.Text.Json.Serialization` namespace to support the `[JsonIgnore]` attribute.

--- Toverland_Api/Models/Area.cs
+++ Toverland_Api/Models/Area.cs
@@ -1,10 +1,13 @@
-﻿namespace Toverland_Api.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Toverland_Api.Models
 {
     public class Area
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public double Size { get; set; }
+        [JsonIgnore] // Exclude Area from serialization
         public List<Attraction>? Attractions { get; set; }
     }
 }
